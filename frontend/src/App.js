6227cbba76e81a98eb36c6d8029bc677a6dba3b7
Aryan Namdev <aryanamdev08@gmail.com>
2023-02-01 09:38:33 +0530

    adding frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+import Form from "./components/Form";
+import Userlist from "./components/Userlist";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Form />
+        <Userlist />
+      </div>
+    </>
   );
 }
 
